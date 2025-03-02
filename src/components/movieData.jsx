17020adb89113e5dcd5d9fd004bfe7cbeddbe430
domeseo <domenico.puzone@gmail.com>
2025-03-02 13:47:17 +0100

    Movie added

--- src/components/movieData.jsx
+++ src/components/movieData.jsx
@@ -1,39 +1,39 @@
 import React, { useState, useEffect } from "react";
 
-const baseUrl = "https://www.swapi.tech/api/people/";
+const baseUrl = "https://www.swapi.tech/api/films/";
 
-function Characters() {
-    const [people, setPeople] = useState([]);
-    const ids = Array.from({ length: 10 }, (_, i) => i + 1); // Crea IDs del 1 al 10
+function Films() {
+    const [movie, setMovie] = useState([]);
+    const ids = Array.from({ length: 5 }, (_, i) => i + 1); // Crea IDs del 1 al 10
 
     useEffect(() => {
-        async function fetchCharacters() {
+        async function fetchFilms() {
             try {
                 const responses = await Promise.all(
                     ids.map(id => fetch(`${baseUrl}${id}`).then(response => response.json()))
                 );
-                const characters = responses.map(data => data.result.properties);
-                setPeople(characters); // Guarda los datos en el estado
+                const film = responses.map(data => data.result.properties);
+                setMovie(film); // Guarda los datos en el estado
             } catch (error) {
                 console.error("Error en el fetch:", error);
             }
         }
 
-        fetchCharacters();
+        fetchFilms();
     }, []); // Se ejecuta solo una vez
 
     return (
         <>
-            <h1>Star Wars Characters</h1>
+            <h1>Star Wars Movies</h1>
             <div style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
-                {people.map((person, index) => (
+                {movie.map((movie, index) => (
                     <div key={index} className="card" style={{ width: "18rem", margin: "10px" }}>
                         <img className="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/2560px-Star_Wars_Logo.svg.png" alt="Character" />
                         <div className="card-body">
-                            <h5 className="card-title">{person.name}</h5>
-                            <p className="card-text"><strong>Gender:</strong> {person.gender}</p>
-                            <p className="card-text"><strong>Height:</strong> {person.height} cm</p>
-                            <p className="card-text"><strong>Birth Year:</strong> {person.birth_year}</p>
+                            <h5 className="card-title">{movie.title}</h5>
+                            <p className="card-text"><strong>Director:</strong> {movie.director}</p>
+                            <p className="card-text"><strong>Year:</strong> {movie.release_date} cm</p>
+                            <p className="card-text"><strong>Episode:</strong> {movie.episode_id}</p>
                         </div>
                     </div>
                 ))}
@@ -42,4 +42,4 @@ function Characters() {
     );
 }
 
-export default Characters;
+export default Films;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import { GetDataStarWarsApi } from "../components/api.jsx";
+import Films from "../components/movieData.jsx";
 import Characters from "../components/peopleData.jsx";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
@@ -10,8 +11,8 @@ export const Home = () => {
 	return (
 		<div className="text-center mt-5">
 
-			<GetDataStarWarsApi />
 			<Characters />
+			<Films /> 
 
 		</div>
 	);
